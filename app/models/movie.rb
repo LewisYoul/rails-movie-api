4c29607b9018e17d97b58923f452d3564c2d5991
Lewis <lewisyoul@gmail.com>
2018-02-20 09:52:22 +0000

    add model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   validates_presence_of :title
+  validates_presence_of :overview
+  validates_presence_of :poster_path
+  validates_presence_of :release_date
+  validates_presence_of :vote_average
 end
--- spec/tester/tester_spec.rb
+++ spec/tester/tester_spec.rb
@@ -11,9 +11,46 @@ describe Movie do
       movie = Movie.new(title: nil)
       expect(movie).to_not be_valid
     end
+    it "is not valid without an overview" do
+      movie = Movie.new(title: "Cars", overview: nil)
+      expect(movie).to_not be_valid
+    end
+    it "is not valid without a poster_path" do
+      movie = Movie.new(title: "Cars", overview: "overview", poster_path: nil)
+      expect(movie).to_not be_valid
+    end
+    it "is not valid without a release_date" do
+      movie = Movie.new(
+        title: "Cars",
+        overview: "overview",
+        poster_path: "path",
+        release_date: nil
+      )
+      expect(movie).to_not be_valid
+    end
+    it "is not valid without a vote_average" do
+      movie = Movie.new(
+        title: "Cars",
+        overview: "overview",
+        poster_path: "path",
+        release_date: "date",
+        vote_average: nil
+      )
+      expect(movie).to_not be_valid
+    end
+    it "is valid when all attributes are given" do
+      movie = Movie.new(
+        title: "Cars",
+        overview: "A summary",
+        poster_path: "route to pic",
+        release_date: "the day it came out",
+        vote_average: 5.4
+      )
+      expect(movie).to be_valid
+    end
   end
 
-  describe "Create Movie" do
+  describe "Create" do
     it "can add a movie to the database" do
       expect { Movie.create(
         title: "Cars",
