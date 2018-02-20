a16fa2d429fd3f0f3f1c3e25072fdbac6a3a81b3
Lewis <lewisyoul@gmail.com>
2018-02-20 09:45:58 +0000

    movie is not valid without a title

--- spec/tester/tester_spec.rb
+++ spec/tester/tester_spec.rb
@@ -6,6 +6,13 @@ describe Movie do
 
   end
 
+  describe "Validations" do
+    it "is not valid without a title" do
+      movie = Movie.new(title: nil)
+      expect(movie).to_not be_valid
+    end
+  end
+
   describe "Create Movie" do
     it "can add a movie to the database" do
       expect { Movie.create(
