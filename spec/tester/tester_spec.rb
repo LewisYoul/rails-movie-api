12651e4cbae422ec954550c7eba6fd13b545077e
Lewis <lewisyoul@gmail.com>
2018-02-20 09:40:27 +0000

    create test for creation of movie

--- spec/tester/tester_spec.rb
+++ spec/tester/tester_spec.rb
@@ -1,20 +1,21 @@
 require_relative '../rails_helper'
 
-describe "a tester" do
-  it "can add two numbers" do
-    expect(2+2).to eq(4)
-  end
-  it "knows what a movie is" do
-    movie = Movie.new(
-      title: "Cars",
-      overview: "A summary",
-      poster_path: "route to pic",
-      release_date: "the day it came out",
-      vote_average: 5.4
-    )
-    p "I AM THE COUNT", Movie.count
-    movie.save
-    p "I AM THE COUNT", Movie.count
+describe Movie do
+
+  before(:each) do
 
   end
+
+  describe "Create Movie" do
+    it "can add a movie to the database" do
+      expect { Movie.create(
+        title: "Cars",
+        overview: "A summary",
+        poster_path: "route to pic",
+        release_date: "the day it came out",
+        vote_average: 5.4
+      )}.to change { Movie.count }
+    end
+  end
+
 end
